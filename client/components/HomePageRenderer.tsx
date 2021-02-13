ab0d7910fb051979b16f865de4218a1a1ff3e5b3
Adrian Inza-Cruz <ainzacruz@gmail.com>
2021-02-12 17:17:21 -0800

    Add files via upload

---
+++ client/components/HomePageRenderer.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+// const HomePageRenderer = () => {
+//   const myHeading: string = 'My Website Heading';
+//   return (
+//     <div>
+//       <h1> {myHeading} </h1>
+//     </div>
+//   )
+// };
+// // const OtherHeading: React.FC = () => {
+// //   return (
+
+// //   )
+
+// // }
+
+// export default HomePageRenderer;

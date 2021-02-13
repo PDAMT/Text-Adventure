e923099a69e4c5e8acddc2d22d53caa3f12110c1
Taylor Davis <moosky@live.com>
2021-02-12 20:50:37 -0700

    adventure text

--- client/components/HomePageRenderer.tsx
+++ client/components/HomePageRenderer.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 
-// const HomePageRenderer = () => {
-//   const myHeading: string = 'My Website Heading';
+const HomePageRenderer = () => {
+  const myHeading: string = 'Adventure Texxxxxt';
+  return (
+    <div>
+      <h1> {myHeading} </h1>
+    </div>
+  )
+};
+// const OtherHeading: React.FC = () => {
 //   return (
-//     <div>
-//       <h1> {myHeading} </h1>
-//     </div>
+
 //   )
-// };
-// // const OtherHeading: React.FC = () => {
-// //   return (
 
-// //   )
+// }
 
-// // }
-
-// export default HomePageRenderer;
+export default HomePageRenderer;

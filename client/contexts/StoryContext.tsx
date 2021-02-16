fb1884698b8398b73ea0e00d0608e5da9714ee53
Taylor Davis <moosky@live.com>
2021-02-15 19:23:22 -0700

    new context

--- client/contexts/StoryContext.tsx
+++ client/contexts/StoryContext.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
-import {useContext} from 'react';
-
+import {  useContext } from 'react';
 interface StoryState {
   prompt: string,
   updatePrompt: void
 }
-
+// axios.get('/prompt', {
+//   params: { "room": room }
+// })
+//   .then((prompt: AxiosResponse) => {
+//     updatePrompt(prompt.data);
+//   })
+//   .catch((err: 'string') => console.log(err));
+  
 const StoryState = {
-  prompt: 'Welcome Players',
-  updatePrompt: 
-}
+  prompt: "Welcome Players";
+};
 
 const StoryContext = React.createContext();
 
-const StoryContextProvider = ({children}) => {
+const StoryContextProvider = ({ children }) => {
   const [prompt, updatePrompt] = React.useState<string | null>('Welcome Players');
   return (
-
+    <StoryContext.Provider value={{prompt, updatePrompt}}>
+      {children}
+    </StoryContext.Provider>
   )
 }

0b356668c73b04a454d2bfeb404f4043907903f8
Taylor Davis <moosky@live.com>
2021-02-16 00:32:29 -0700

    ok

--- client/components/prompt.tsx
+++ client/components/prompt.tsx
@@ -30,7 +30,7 @@ const Prompt = () => {
   const classes = useStyles();
   const { prompt, updatePrompt , room} = useContext(StoryContext);
   const getPromptData = () => {
-    const roomStr: string = room.toString(); 
+    const roomStr: stri = room.toString(); 
     axios.get(`/api/prompts/${roomStr}`)
       .then((res: AxiosResponse) => {
         const { prompt } = res.data[0];

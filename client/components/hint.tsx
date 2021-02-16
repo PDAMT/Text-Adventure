03f05df46e1e97ce0b1ab6b8e22f00ec8a701554
Phillip Kekoa Bannister <phillip.kbannister@gmail.com>
2021-02-15 22:14:55 -0800

    hint will update state on fetch request

--- client/components/hint.tsx
+++ client/components/hint.tsx
@@ -17,19 +17,11 @@ const Hint = () => {
     fetch("http://localhost:3000/api/hints/1")
       .then((response:any) => {
         console.log("about to parse")
-        console.log(response)
+        return response.json();
       })
-      .then(result => console.log("hey I got here",result))
+      .then(result => setHint(result[0].hint))
       .catch(error => console.log('error', error));
   });
-    // const myHeaders = new Headers();
-    // myHeaders.append("Content-Type", "application/json"); 
-
-    // const requestOptions: Get = {
-    //   method: 'GET',
-    //   headers: myHeaders,
-    // }
-
 
   return (
   <div className="box">
@@ -39,33 +31,3 @@ const Hint = () => {
   )
 }
 export default Hint;
-
-    // //  React.useEffect(() => {
-// //     const roomNum: string = '1'; 
-// //     const dbQuery = '/api/prompts/' + roomNum; 
-
-    // axios.get(dbQuery)
-    //   .then((res: AxiosResponse) => {
-    //     const { prompt } = res.data[0];
-    //     console.log(' updated', prompt)
-    //     updatePrompt(prompt); 
-    //   })
-    //   .catch((err: 'string') => console.log(err));
-    // },[]);
-
-        
-    // const options: Get = {
-    //   method: "GET",
-    //   data:{},
-    //   headers:{},
-    //   params: {"room": room },
-    // }
-
-    // Axios.get(`/api/hints/1`)
-    //   .then((response: AxiosResponse) => {
-    //     console.log("==============response object below==================")
-    //     console.log("🚀 ~ file: Hint.tsx ~ line 29 ~ .then ~ response", response)
-    //     setHint(response.data)
-    //   })
-    //   .catch(err => console.log(err));
-    // }, []);
